0326d42a5cc5e805a2b1f5a290b37457608e5ead
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-11-20 19:13:30 +0900

    up data

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,6 +1,66 @@
-$(function(){
-  $("#user-search-field").on("keyup",function(){
+$(function() {
+
+  function appendUser(user){
+    var html = `
+              <div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${user.name}</p>
+                <div class="user-search-add chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加<div> 
+    search_list.append(html);
+              }
+
+  function appendNouser(user){
+    var html = `<div class='chat-group-user clearfix'>
+                  <p class='chat-group-user__name'>${user}</p>
+                  </div>
+    search_list.append(html);
+`}
+
+
+  $("#user-search-field").on("keyup", function() {
     let input = $("#user-search-field").val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+      .done(function(users) {
+        console.log("成功です");
+      })
+      .fail(function() {
+        console.log("失敗です");
+      });
   });
 });
+
+$(function(){
+  function addUser(user) {
+    let html = `
+      <div class="chat-group-user clearfix">
+        <p class="chat-group-user__name">${user.name}</p>
+        <div class="user-serach-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+      </div> 
+    `;
+    $("#user-search-result").append(html);
+  }
+  function addDeleteUser(name, id) {
+    let html = `
+    <div class="ChatMember clearfix" id="${id}">
+      <p class= "ChatMemer__name">${name}</p>
+      <div class="ChatMember__remove ChatMember__button" data-user-id="${id}" data-user-name="${name}">削除</div>
+      </div>;
+      $(".ChatMembers").append(html);
+  }
+  $("#user-search-field").on("keyup", function(){
+    let input = $("#user-search-field").val();
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: "json"
+    })
+    .done(function(users){
+      $("#user-search-result").empty()
+    })
+  })
+})
