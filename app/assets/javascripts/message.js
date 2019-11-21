91c723acc4d4e4245a9497f5662f79a1abcf53c9
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-11-21 17:59:01 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,44 +3,23 @@ $(function(){
   console.log(last_message_id);
 
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
+    var html = 
+      `<div class=message>
+        <div class="upper-message">
+          <div class="upper-message__user-name">
+            ${message.user_name}　
+          </div>
+          <div class="upper-message__date">
+            ${message.date}
+          </div>
+        </div>
+        <div class="lower-message">
+          <p class="lower-message__content">
+            ${message.content}
+          </p>
+          ${image}
+        </div>
+      </div> `
  }
 $('#new_message').on('submit', function(e){
  e.preventDefault();
