af50bfa5ed0678841009f4751aac6aa61b9e8cbc
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-11-25 16:48:51 +0900

    up

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -21,17 +21,17 @@ $(function(){
     return html;
   }
   $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  let formData = new FormData(this);
-  let url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
-  })
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
     .done(function(data){
       let html = buildHTML(data);
       $('.messages').append(html);
@@ -45,29 +45,29 @@ $(function(){
   });
 
 
-    var reloadMessages = function(){
-      var last_message_id = $('.messages').filter(":last").data('messageId')
-      if (window.location.href.match(/\/groups\/\d+\/messages/)){
+  var reloadMessages = function(){
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
       $.ajax({
         url: "api/messages",
-        data: { last_id: last_message_id },
-        type: "GET",
-        dataType: 'json'
+        type: "get",
+        dataType: 'json',
+        data: { last_id: last_message_id }
       })
       .done(function(data){
         var insertHTML = '';
         data.forEach(function(message){
-        insertHTML = buildHTML(message);         
-        $('.main__message').append(insertHTML)
-        if (last_message_id < message.id){
-          $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
-        }
-      });
-    })
-    .fail(function(data){
-      alert('自動更新に失敗しました');
-    })
-  } else{console.log(this);}
-};
+          insertHTML = buildHTML(message);         
+          $('.main__message').append(insertHTML)
+          if (last_message_id < message.id){
+            $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+          }
+        });
+      })
+      .fail(function(data){
+        alert('自動更新に失敗しました');
+      })
+    } else{console.log(this);}
+  };
 setInterval(reloadMessages, 7000);
 });
