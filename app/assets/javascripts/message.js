73af4b94eca51c57567ade38b4e343e9bf5800ee
5ba8shi <55795442+5ba8shi@users.noreply.github.com>
2019-11-30 14:42:25 +0900

    op

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,7 +42,7 @@ $(function(){
   }
 
   $('#new_message').on('submit', function(e){
-    e.preventDefault()
+    e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
     $.ajax({
